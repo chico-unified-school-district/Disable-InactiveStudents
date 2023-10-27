8ff4d1218e614fec38450bb1141fcfd9840c50e1
Justin Cooper <jcooper@chicousd.org>
2023-10-27 11:28:33 -0700

    logging cleaned up

--- Disable-InactiveStudents.ps1
+++ Disable-InactiveStudents.ps1
@@ -233,7 +233,7 @@ function Set-ChromebookOU {
  process {
   $id = $_.deviceId
   if ($_.orgUnitPath -match $targOu) { return } # Skip is OU is correct
-  $msg = $MyInvocation.MyCommand.name, $_.sn, "& $gam update cros $id ou $targOu"
+  $msg = $MyInvocation.MyCommand.name, $_.serialNumber, "& $gam update cros $id ou $targOu"
   Write-Host ('{0},[{1}],[{2}]' -f $msg) -F $update
   if ($WhatIf) { return }
   & $gam update cros $id ou $targOu *>$null
@@ -245,7 +245,7 @@ function Disable-Chromebook {
  process {
   $id = $_.deviceId
   if ($crosDev.status -ne "ACTIVE") { return }
-  $msg = $MyInvocation.MyCommand.name, $_.sn, "& $gam update cros $id action disable"
+  $msg = $MyInvocation.MyCommand.name, $_.serialNumber, "& $gam update cros $id action disable"
   Write-Host ('{0},[{1}],[{2}]' -f $msg) -F DarkCyan
   if ($WhatIf) { return }
   & $gam update cros $id action disable *>$null
